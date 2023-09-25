eb897396ae8609ad813e603a005e8da43a559703
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2023-09-26 06:54:34 +1000

    added GET books endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 
@@ -14,13 +15,25 @@ type Book struct {
 	Year   string `jason:year`
 }
 
+var books []Book
+
 func main() {
 	r := mux.NewRouter()
+	books = append(books, Book{ID: "1", Title: "Book 1", Author: "Author 1", Year: "1991"},
+		Book{ID: "2", Title: "Book 2", Author: "Author 2", Year: "1992"},
+		Book{ID: "3", Title: "Book 3", Author: "Author 3", Year: "1993"},
+		Book{ID: "4", Title: "Book 4", Author: "Author 4", Year: "1994"})
+
 	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/books", getBooks).Methods("GET")
 	http.ListenAndServe(":8080", r)
 }
 
-func HomeHandler(writer http.ResponseWriter, request *http.Request) {
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(books)
+}
 
-	io.WriteString(writer, "Hello World!")
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+
+	io.WriteString(w, "Hello World!")
 }
